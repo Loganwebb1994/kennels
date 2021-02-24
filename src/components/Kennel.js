42504638689b6fe1939ff7d992e0c4659186c389
Loganwebb1994 <loganwebb1994@gmail.com>
2021-02-24 15:33:47 -0600

    added objects to the dom with react for the first time

--- src/components/Kennel.js
+++ src/components/Kennel.js
@@ -1,4 +1,5 @@
 import React from "react"
+import { AnimalCard } from "./animal/AnimalCard"
 import "./Kennel.css"
 export const Kennel = () => (
     <>
@@ -8,5 +9,11 @@ export const Kennel = () => (
             <div>Visit Us at the Nashville North Location</div>
             <div>500 Puppy Way</div>
         </address>
+        <h2>Animals</h2>
+        <article className="animals">
+            <AnimalCard />
+            <AnimalCard />
+            <AnimalCard />
+        </article>
     </>
 )
