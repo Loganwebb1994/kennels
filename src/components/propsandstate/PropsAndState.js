21b7a5186244ac1e2afaa7d21798ef7b01d10161
Loganwebb1994 <loganwebb1994@gmail.com>
2021-02-25 14:29:13 -0600

    added a button that increments an integer on the dom

--- src/components/propsandstate/PropsAndState.js
+++ src/components/propsandstate/PropsAndState.js
@@ -1,11 +1,18 @@
 import "./PropsAndState.css"
-import React from "react"
+import React, {useState} from "react"
 
 export const PropsAndState = ({ yourName }) => {
+    let [countClicks, setCountClicks] = useState(0)
+    const handleClick = () =>{
+        const newCountClicks = ++countClicks
+        setCountClicks(newCountClicks)
+    }
 
     return (
     <>
         <h3>Welcome, {yourName} </h3>
+        <p>{countClicks}</p>
+        <button onClick={(handleClick)}>Increment</button>
     </>
     )
 }
