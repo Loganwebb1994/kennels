1f5bd131a52ff7abcbef9ba814343aca441b1832
Loganwebb1994 <loganwebb1994@gmail.com>
2021-02-25 15:23:39 -0600

    finished chapter four

--- src/Kennel.js
+++ src/Kennel.js
@@ -1,42 +1,11 @@
 import React from "react"
-import { AnimalCard } from "./components/animal/AnimalCard"
-import { Customer } from "./components/customers/Customer"
-import {Employee} from "./components/employees/Employee.js"
-import {Location} from "./components/locations/Location.js"
-import { PropsAndState } from "./components/propsandstate/PropsAndState"
+import { NavBar } from "./components/nav/NavBar"
+import { ApplicationViews } from "./components/ApplicationViews"
 import "./Kennel.css"
+
 export const Kennel = () => (
     <>
-        <h2>Nashville Kennels</h2>
-        <small>Loving care when you're not there.</small>
-        <address>
-            <div>Visit Us at the Nashville North Location</div>
-            <div>500 Puppy Way</div>
-        </address>
-        <PropsAndState yourName="Logan"/>
-        <h2>Animals</h2>
-        <article className="animals">
-            <AnimalCard />
-            <AnimalCard />
-            <AnimalCard />
-        </article>
-        <h2>Employees</h2>
-        <article className="employees">
-            <Employee />
-            <Employee />
-            <Employee />
-        </article>
-        <h2>Locations</h2>
-        <article className="locations">
-            <Location />
-            <Location />
-        </article>
-        <h2>Customers</h2>
-        <article className="customers">
-            <Customer />
-            <Customer />
-            <Customer />
-            <Customer />
-        </article>
+        <NavBar />
+        <ApplicationViews />
     </>
 )
