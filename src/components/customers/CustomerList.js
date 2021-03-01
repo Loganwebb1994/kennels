cd14991b9e6744078d0999f785201cbcd61684ee
Loganwebb1994 <loganwebb1994@gmail.com>
2021-03-01 11:16:49 -0600

    The app is now pulling customers from my api and rendering them on the DOM

---
+++ src/components/customers/CustomerList.js
@@ -0,0 +1,31 @@
+import React, { useContext, useEffect } from "react"
+import { CustomerContext } from "./CustomerProvider"
+import { Customer } from "./Customer"
+import "./Customer.css"
+
+export const CustomerList = () => {
+  // This state changes when `getAnimals()` is invoked below
+  const { customers, getCustomers } = useContext(CustomerContext)
+
+  //useEffect - reach out to the world for something
+  useEffect(() => {
+    console.log("AnimalList: useEffect - getAnimals")
+    getCustomers()
+
+  }, [])
+
+
+    return (
+    <>
+    <h2>Customers</h2>
+    <div className="customers">
+        {console.log("CustomerList: render", customers)}
+        {
+        customers.map(customer => {
+            return <Customer key={customer.id} customer={customer} />
+        })
+        }
+    </div>
+    </>
+    )
+}
