0577564750feea18182caa652359b03ea2d9654b
Loganwebb1994 <loganwebb1994@gmail.com>
2021-03-01 14:51:24 -0600

    used multiple providers in one component

--- src/components/animal/AnimalList.js
+++ src/components/animal/AnimalList.js
@@ -1,16 +1,21 @@
 import React, { useContext, useEffect } from "react"
 import { AnimalContext } from "./AnimalProvider"
+import { LocationContext } from "../locations/LocationProvider"
+import { CustomerContext } from "../customers/CustomerProvider"
 import { AnimalCard } from "./AnimalCard"
 import "./Animal.css"
 
 export const AnimalList = () => {
   // This state changes when `getAnimals()` is invoked below
   const { animals, getAnimals } = useContext(AnimalContext)
-
+  const { locations, getLocations } = useContext(LocationContext)
+  const { customers, getCustomers } = useContext(CustomerContext)
   //useEffect - reach out to the world for something
   useEffect(() => {
     console.log("AnimalList: useEffect - getAnimals")
-    getAnimals()
+    getLocations()
+    .then(getCustomers)
+    .then(getAnimals)
 
   }, [])
 
@@ -19,11 +24,16 @@ export const AnimalList = () => {
     <>
     <h2>Animals</h2>
     <div className="animals">
-        {console.log("AnimalList: Render", animals)}
         {
         animals.map(animal => {
-            return <AnimalCard key={animal.id} animal={animal} />
-        })
+          const owner = customers.find(c => c.id === animal.customerId)
+          const location = locations.find(l => l.id === animal.locationId)
+      
+          return <AnimalCard key={animal.id}
+                      location={location}
+                      customer={owner}
+                      animal={animal} />
+      })
         }
     </div>
     </>
