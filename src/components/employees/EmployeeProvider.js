6f3f78497f46ed19b38da894963e2129d67d1437
Loganwebb1994 <loganwebb1994@gmail.com>
2021-03-05 13:27:35 -0600

    added function to employee provider to edit employee

--- src/components/employees/EmployeeProvider.js
+++ src/components/employees/EmployeeProvider.js
@@ -28,15 +28,21 @@ export const EmployeeProvider = (props) => {
         return fetch(`http://localhost:8088/employees/${id}?_expand=location`)
             .then(res => res.json())
     }
-    /*
-        You return a context provider which has the
-        `animals` state, `getAnimals` function,
-        and the `addAnimal` function as keys. This
-        allows any child elements to access them.
-    */
+    
+    const updateEmployee = employee => {
+        return fetch(`http://localhost:8088/employees/${employee.id}`, {
+        method: "PUT",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(employee)
+        })
+        .then(getEmployees)
+    }
+
     return (
         <EmployeeContext.Provider value={{
-            employees, getEmployees, addEmployees, getEmployeeById,
+            employees, getEmployees, addEmployees, getEmployeeById, updateEmployee
         }}>
             {props.children}
         </EmployeeContext.Provider>
